2066fd83ec1fa43d71903c62ed5533cb505c58ff
Mazzya <41023812+Mazzya@users.noreply.github.com>
2020-09-03 19:43:59 +0200

    Version 1.0.1 BETA

---
+++ Files/dist/main/modules.py
@@ -0,0 +1,5 @@
+# This module contains all the imports necessary for the operation of the program.
+
+from tkinter import *
+from validate_email import validate_email
+from tkinter import messagebox as MessageBox
